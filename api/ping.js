9af46aaa970cba332b707607c4dc175f6f3110ee
ibarulin <barulini@gmail.com>
2025-10-22 23:59:59 +0300

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,7 @@
 export default function handler(req, res) {
-  res.status(200).json({ ok: true, time: Date.now() });
+  res.status(200).json({
+    ok: true,
+    CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || null,
+    CLOUDINARY_UPLOAD_PRESET: process.env.CLOUDINARY_UPLOAD_PRESET || null
+  });
 }
